d5a061bf93f90b11223b0ccbd6af0475b2b88963
Sindri Thor <sindrihar@hotmail.com>
2022-09-13 16:19:58 +0000

    Set up basic endpoints for getting Clients, Barbers, and Appointments from the cloud Database

--- src/app/dist/app.js
+++ src/app/dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,11 +17,24 @@ const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.use(express_1.default.static(path_1.default.join(__dirname, '/../pages/')));
-app.get('/', (req, res) => {
-    // send index.html from pages
-    //res.sendFile('../pages/index.html')
-    //res.send('Hello World!');
-});
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const URI = "mongodb+srv://Demo:Nsi8yQYWYYQXc5GM@hugb-hopur5.59nlce1.mongodb.net/BarberShop?retryWrites=true&w=majority";
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect();
+const Database = client.db("BarberShop");
+app.get('/Users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const Users = yield Database.collection("Users").find({}).toArray();
+    return res.status(200).json(Users);
+}));
+app.get('/Appointments', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const Users = yield Database.collection("Appointments").find({}).toArray();
+    return res.status(200).json(Users);
+}));
+app.get('/Barbers', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const Users = yield Database.collection("Barbers").find({}).toArray();
+    return res.status(200).json(Users);
+}));
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
--- src/app/src/app.ts
+++ src/app/src/app.ts
@@ -1,14 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '/../pages/')));
 
-app.get('/', (req, res) => {
-  // send index.html from pages
-  //res.sendFile('../pages/index.html')
-  //res.send('Hello World!');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const URI = "mongodb+srv://Demo:Nsi8yQYWYYQXc5GM@hugb-hopur5.59nlce1.mongodb.net/BarberShop?retryWrites=true&w=majority"
+const client = new MongoClient(URI,{ useNewUrlParser: true, useUnifiedTopology: true });
+client.connect();
+const Database = client.db("BarberShop")
+
+
+app.get('/Users', async (req: Request, res: Response) => {
+  const Users:JSON = await Database.collection("Users").find({}).toArray();
+  return res.status(200).json(Users);
+});
+
+
+app.get('/Appointments', async (req: Request, res: Response) => {
+  const Users:JSON = await Database.collection("Appointments").find({}).toArray();
+  return res.status(200).json(Users);
+});
+
+app.get('/Barbers', async (req: Request, res: Response) => {
+  const Users:JSON = await Database.collection("Barbers").find({}).toArray();
+  return res.status(200).json(Users);
 });
 
 app.listen(port, () => {
