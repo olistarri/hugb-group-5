e4d2332a5c26ba26d72c007fdd10920a9c0fa889
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-09-17 18:31:44 +0000

    ATH Breytti references til baka

---
+++ src/app/static/js/choose_service.js
@@ -0,0 +1,9 @@
+var haircut = document.getElementById("haircut-book-button");
+var shave = document.getElementById("shave-book-button");
+var coulouring = document.getElementById("colouring-book-button");
+
+haircut.addEventListener("change" , function() {
+    console.log("Pressed")
+})
+
+console.log("asdf")
