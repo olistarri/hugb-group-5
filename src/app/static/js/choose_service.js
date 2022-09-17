b507f81c6e23d4c6f7f851ab8501b301bfa6e3c2
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-09-17 19:01:37 +0000

    booking_confirm_1

--- src/app/static/js/choose_service.js
+++ src/app/static/js/choose_service.js
@@ -2,8 +2,4 @@ var haircut = document.getElementById("haircut-book-button");
 var shave = document.getElementById("shave-book-button");
 var coulouring = document.getElementById("colouring-book-button");
 
-haircut.addEventListener("change" , function() {
-    console.log("Pressed")
-})
-
-console.log("asdf")
+// Ekki tilbúið, þarf bara að bætta í sessionstorage hvað var valið
